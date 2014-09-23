22a176c4ef98b68e3fa568d99bf082353206f86a
Juan Matthys Uys <opyate@gmail.com>
2014-09-23 10:42:01 +0100

    traverse previous versions

--- application/model.py
+++ application/model.py
@@ -25,12 +25,26 @@ class Meta(object):
     """Meta-data about the thing being stored."""
 
     def __init__(self, key):
-        link = os.environ['HOST'] + key.name + "?version=" + key.version_id
         self.meta = {
             'version_id': key.version_id,
-            'last_modified': key.last_modified,
-            "http://schema.org/url": {"@id": link}
+            'last_modified': key.last_modified
         }
+        self.meta.update(VersionedLink(key.name, key.version_id).as_dict())
+
+        # meta data currently only carries 'previous version' information
+        if key.metadata:
+            previous_version_id = key.metadata['previous_version_id']
+            previous = {'version_id': previous_version_id}
+            previous.update(VersionedLink(key.name, previous_version_id).as_dict())
+            self.meta.update({'previous': previous})
 
     def as_dict(self):
         return self.meta
+
+class VersionedLink(object):
+
+    def __init__(self, name, version_id):
+        self.link = os.environ['HOST'] + name + "?version=" + version_id
+
+    def as_dict(self):
+        return {"http://schema.org/url": {"@id": self.link}}
--- application/storage.py
+++ application/storage.py
@@ -17,7 +17,14 @@ class S3Store(object):
         self.bucket = bucket
 
     def post(self, key, data):
+        # the new object to persist
         obj = Key(self.bucket)
+
+        # get the previous version, if any
+        previous = self.get(key, None)
+        if previous:
+            obj.metadata = {'previous_version_id': previous.version_id}
+
         obj.key = key
         obj.set_contents_from_string(json.dumps(data))
 
