2a1b5a81fbc1907756339dd4895116eebbbdc5b5
Juan Matthys Uys <opyate@gmail.com>
2014-09-23 14:27:35 +0100

    scripts for dev env

---
+++ environment.sh
@@ -0,0 +1,4 @@
+export HOST='http://historian.landregistry.local/'
+export S3_BUCKET='land-registry-title-archive'
+export AWS_ACCESS_KEY_ID="set this elsewhere"
+export AWS_SECRET_ACCESS_KEY="set this elsewhere"
